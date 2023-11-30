d9ef47765194b6de6293087cfbb557a363f06fd4
Rosie Miall <rosiemiall38@gmail.com>
2023-11-30 10:11:16 +0000

    all extension complete: MORE css

--- src/components/Cake.js
+++ src/components/Cake.js
@@ -7,16 +7,18 @@ const Cake = ({ cake, onButtonClick, earnings }) => {
   return (
     <>
       <box id="box">
-        <hr />
+        
         <h3 id="names">{cake.cakeName}</h3>
+        <hr />
         <ul>Ingredients: {cake.ingredients.map(ingredient=> {return <li>{ingredient}</li>} )}</ul>
       
         <p>Price: £{cake.price} </p>
         <p>Rating: {cake.rating}</p>
+        <hr />
         <button id="button" onClick={buyCake}>
           Sell Cake
         </button>
-        <hr />
+        
       </box>
     </>
   );
